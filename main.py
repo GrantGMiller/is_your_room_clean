ccb0a224f12bd8a8bae70ee155e942ea5e3dfe5a
Grant Miller <grant.miller@sas.com>
2026-08-19 16:31:34 -0400

    added test endpoint

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+import time
+
 from flask import Flask
 
 app = Flask('Is Your Room Clean')
+
+
+@app.route('/test')
+def test():
+    return 'The time is ' + time.asctime()
+
+
+if __name__ == '__main__':
+    app.run(port=3888, debug=True)
